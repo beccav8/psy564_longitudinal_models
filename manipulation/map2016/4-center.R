7138eb8a3d632bd27e1a56eff448ea923880e994
Rebecca <rebecca04v@gmail.com>
2016-10-20 21:28:23 -0700

    #centered

--- manipulation/map2016/4-center.R
+++ manipulation/map2016/4-center.R
@@ -29,26 +29,6 @@ head(dto)
 data<-dto
 
 
-
-set.seed(1)
-ids <- sample(data$id,20)
-d <- data %>%  dplyr::filter( id %in% ids)
-names(d)
-
-p1 <- ggplot(d, aes(x=year_in_study, y=physical_activity, group=id)) +
-  geom_line() +
-  # stat_smooth(method=lm, se=FALSE)+
-  scale_color_brewer(palette="Set2") +
-  # geom_text(aes(label=id))+
-  ggtitle("Growth curve for individuals")
-p1
-
-
-library(lattice)
-xyplot(physical_activity ~ year_in_study | id, data=d, as.table=T)
-
-
-
 ##########################-----Center-PA-----#########################################################
 ##----physical-activity-between-person---MEAN------------------------------------------------------------
 # phys_bp = the persons mean on PA across occasions - the grand mean of PA in the population  (mean=2.94)
@@ -59,23 +39,28 @@ for(i in unique(data$id)) {
     data$phys_bp_mean[data$id==i][j]<- ( (data$physical_activity[data$id==i][j]) - (mean(data$physical_activity, na.rm =TRUE)) ) 
     
   } }
-
+#mean=2.94
 data$physical_activity[data$id==21305588]
 data$phys_bp_mean[data$id==21305588]
+ids <- sample(unique(data$id),3)
+data %>%
+  dplyr::filter(id %in% ids ) %>%
+  dplyr::group_by(id) %>%
+  dplyr::select(id,physical_activity,phys_bp_mean)
 
 
-# ##----physical-activity-between-person---MEDIAN------------------------------------------------------------
-# # phys_bp = the persons median on PA across occasions - the grand median of the population  (median = 1.75)
-# 
-# for(i in unique(data$id)) {
-#   for (j in 1:length(data$physical_activity[data$id==i])) {
-#     
-#     data$phys_bp_median[data$id==i][j]<- ( (data$physical_activity[data$id==i][j]) - (median(data$physical_activity, na.rm =TRUE)) ) 
-#     
-#   } }
-# 
-# data$physical_activity[data$id==21305588]
-# data$phys_bp_median[data$id==21305588]
+##----physical-activity-between-person---MEDIAN------------------------------------------------------------
+# phys_bp = the persons score on PA at that occasion - the grand median of the population  (median = 1.75)
+
+for(i in unique(data$id)) {
+  for (j in 1:length(data$physical_activity[data$id==i])) {
+
+    data$phys_bp_median[data$id==i][j]<- ( (data$physical_activity[data$id==i][j]) - (median(data$physical_activity, na.rm =TRUE)) )
+
+  } }
+
+data$physical_activity[data$id==21305588]
+data$phys_bp_median[data$id==21305588]
 
 
 ##----physical-activity-within-person-fluctiations-MEAN-CENTERED---------------------------------------------------
@@ -95,24 +80,30 @@ data$phys_wp[data$id==21305588]
 
 #######################################-center-age-#################################################
 
-mean(data$age_at_visit, na.rm=TRUE) 83
+#center at mean age-------------------------
 
-data$age_at_visit_centered<- (data$age_at_visit) - (mean(data$age_at_visit, na.rm=TRUE))
+mean(data$age_at_visit, na.rm=TRUE) #83.15
+data$age_at_visit_meanc<- (data$age_at_visit) - (mean(data$age_at_visit, na.rm=TRUE))
 
 
 data$age_at_visit[data$id==21305588]
-data$age_at_visit_centered[data$id==21305588]
+data$age_at_visit_meanc[data$id==21305588]
+
+#center at arbitrary meaningful number-----------------------
+
+range(data$age_at_visit, na.rm=TRUE) #53-107
+histogram(data$age_at_visit)
+#choose 65
+
+data$age_at_visit65<- (data$age_at_visit) - (65)
 
 
-#-------------------------- centering baseline age
+data$age_at_visit[data$id==21305588]
+data$age_at_visit65[data$id==21305588]
 
-# mean_age_baseline <- mean(data[["age_bl"]], na.rm = TRUE)   #79
-# est_mean_age_bl<- rep(mean(data$age_bl, na.rm=TRUE), length(data$age_bl))
-# data$age_bl_centered<- data$age_bl - est_mean_age_bl
 
 names(data)
 
 
 saveRDS(data, "./data/unshared/derived/map2016/data_centered.rds")
-
 
